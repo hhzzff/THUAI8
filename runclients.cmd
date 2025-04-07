52696f21f993df2d14dd2985f59d70344b9ef0d4
hhzzff <2070620600@qq.com>
2025-04-07 20:42:45 +0800

    test:

--- runclients.cmd
+++ runclients.cmd
@@ -1,4 +1,3 @@
-start logic\Server\bin\Debug\net8.0\Server.exe --port 8888 --CharacterNum 1
 start logic\ClientTest\bin\Debug\net8.0\ClientTest.exe 0 1
 start logic\ClientTest\bin\Debug\net8.0\ClientTest.exe 0 0
 start logic\ClientTest\bin\Debug\net8.0\ClientTest.exe 1 1
---
+++ runserver.cmd
@@ -0,0 +1,1 @@
+start logic\Server\bin\Debug\net8.0\Server.exe --port 8888 --CharacterNum 1
